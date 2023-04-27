a903129105cba615092ce5d264c9eabe594f09ea
Hrishit-B <hrishitb0506@gmail.com>
2023-04-27 10:35:11 +0530

    commit

--- Serwer.js
+++ Serwer.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose');
 
 const DB = 'mongodb+srv://hrishitb0506:bHyty9Xdf7uvN3fj@cluster0.gqxxnd5.mongodb.net/test';
 
-// mongoose
-//     .connect(DB)
-//     .then(()=> {
-//         console.log('DB successfully connected');
-//     });
+mongoose
+    .connect(DB)
+    .then(()=> {
+        console.log('DB successfully connected');
+    });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
-    console.log('DB successfully connected');
 });
